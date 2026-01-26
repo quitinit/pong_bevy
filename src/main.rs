8db692b7401ebc99837893679fe58ad68c084585
Michael Dinitz <michaeldinitz@yahoo.de>
2026-01-26 18:42:55 +0100

    somewhat working game, no direction changes yet but player control and collision detection

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
-use bevy::{color::palettes::basic::WHITE, ecs::query, prelude::*};
+use bevy::{
+    color::palettes::basic::WHITE,
+    ecs::query,
+    math::bounding::{Aabb2d, IntersectsVolume},
+    mesh,
+    prelude::*,
+};
 
 #[derive(Component)]
 struct Player;
@@ -22,13 +28,15 @@ struct Score(u8);
 #[derive(Resource)]
 struct Velocity(f32);
 
+#[derive(Component)]
+struct Collider;
 fn move_rectangle(
     time: Res<Time>,
     mut query: Query<&mut Transform, With<Ball>>,
     velocity: Res<Velocity>,
 ) {
     for mut transform in &mut query {
-        transform.translation.x += 300. * velocity.0 * time.delta_secs();
+        transform.translation.x += (300. * velocity.0) * time.delta_secs();
     }
 }
 
@@ -46,7 +54,7 @@ fn keyboard_control(
                 transform.translation.y += 5.
             }
         }
-    }
+    };
     if keyboard_input.pressed(KeyCode::KeyS) {
         for mut transform in &mut player_query {
             println!("window size is {}", window.height());
@@ -55,21 +63,58 @@ fn keyboard_control(
                 transform.translation.y -= 5.
             }
         }
-    }
+    };
 }
-fn collision(
+fn oponent_keyboard_control(
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    mut player_query: Query<&mut Transform, With<Oponent>>,
+    window: Single<&Window>,
+) {
+    if keyboard_input.pressed(KeyCode::ArrowUp) {
+        for mut transform in &mut player_query {
+            println!("window size is {}", window.height());
+            println!("y position is {}", transform.translation.y);
+
+            if transform.translation.y <= window.height() / 2. - 52. {
+                transform.translation.y += 5.
+            }
+        }
+    };
+    if keyboard_input.pressed(KeyCode::ArrowDown) {
+        for mut transform in &mut player_query {
+            println!("window size is {}", window.height());
+            println!("y position is {}", transform.translation.y);
+            if transform.translation.y > -window.height() / 2. + 52. {
+                transform.translation.y -= 5.
+            }
+        }
+    };
+}
+
+fn detect_collision(
     ball_query: Query<&Transform, With<Ball>>,
-    paddle_query: Query<&Transform, With<Paddle>>,
+    collider_query: Query<&Transform, With<Collider>>,
     mut velocity: ResMut<Velocity>,
 ) {
     for transform in ball_query {
-        let paddle_translate = paddle_query.single().unwrap().translation.x;
-        println!(
-            "transform of ball is {} and of paddle {}",
-            transform.translation.x, paddle_translate
+        let ball_bounding_box = Aabb2d::new(
+            Vec2 {
+                x: transform.translation.x,
+                y: transform.translation.y,
+            },
+            Vec2 { x: 16.0, y: 16.0 },
         );
-        if transform.translation.x == paddle_translate {
-            velocity.0 *= -1.
+        for collider_transform in collider_query {
+            let collider_bounding_box = Aabb2d::new(
+                Vec2 {
+                    x: collider_transform.translation.x,
+                    y: collider_transform.translation.y,
+                },
+                Vec2 { x: 8., y: 50.0 },
+            );
+            if ball_bounding_box.intersects(&collider_bounding_box) {
+                velocity.0 *= -1.
+            }
         }
     }
 }
@@ -83,7 +128,6 @@ fn setup(
     commands.spawn(Camera2d);
     commands.spawn((
         Ball,
-        Paddle,
         Mesh2d(meshes.add(Rectangle::default())),
         MeshMaterial2d(materials.add(Color::from(WHITE))),
         Transform::default().with_scale(Vec3::splat(32.0)),
@@ -91,6 +135,7 @@ fn setup(
     commands.spawn((
         Oponent,
         Paddle,
+        Collider,
         Mesh2d(meshes.add(Rectangle::default())),
         MeshMaterial2d(materials.add(Color::from(WHITE))),
         Transform::default()
@@ -99,6 +144,8 @@ fn setup(
     ));
     commands.spawn((
         Player,
+        Paddle,
+        Collider,
         Mesh2d(meshes.add(Rectangle::default())),
         MeshMaterial2d(materials.add(Color::from(WHITE))),
         Transform::default()
@@ -106,6 +153,7 @@ fn setup(
             .with_translation(Vec3::new(-window.width() / 2. + 32., 0., 0.)),
     ));
 }
+
 fn main() {
     App::new()
         .insert_resource(Velocity(1.0))
@@ -113,7 +161,13 @@ fn main() {
         .add_systems(Startup, setup)
         .add_systems(
             Update,
-            (keyboard_control, move_rectangle, collision).chain(),
+            (
+                keyboard_control,
+                oponent_keyboard_control,
+                move_rectangle,
+                detect_collision,
+            )
+                .chain(),
         )
         .run();
 }
